6796b9c9ed632badf8c6aaf4375751e3842af70c
Muhammad Umair Tahir <nobelhighschool@hotmail.com>
2021-05-11 13:37:28 +0500

    fixed token expiry issue

--- TechFlurry.Authorization.WebAssembly/Abstractions/IAuthenticationService.cs
+++ TechFlurry.Authorization.WebAssembly/Abstractions/IAuthenticationService.cs
@@ -1,6 +1,11 @@
-﻿namespace TechFlurry.Authorization.WebAssembly.Abstractions
+﻿using System.Threading.Tasks;
+using TechFlurry.Authorization.WebAssembly.Models;
+
+namespace TechFlurry.Authorization.WebAssembly.Abstractions
 {
     public interface IAuthenticationService
     {
+        Task<AuthenticatedUser> Login(Credentials credentials);
+        Task Logout();
     }
 }
